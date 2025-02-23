44af1428b25b74627709ad08a6e45594d2665bf9
Foysal79 <onthor312001@gmail.com>
2025-02-23 08:27:06 +0600

    underrstanding Eroor Patterns in Zod And Mongoose

--- src/app/modules/middlwares/globalErrorHandler.ts
+++ src/app/modules/middlwares/globalErrorHandler.ts
@@ -10,17 +10,51 @@
 //   }
 //   export default globalErrorHandler
 
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response } from 'express';
+import { ZodError } from 'zod';
 
+const globalErrorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
 
-const globalErrorHandler = (err : any, req : Request, res : Response, next : NextFunction) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-      message,
-      success : false,
-      status: err,
-     })
-}
+  type TErrorSource = {
+    path: string | number;
+    message: string;
+  }[];
 
-export default globalErrorHandler
+  const errorSources: TErrorSource = [
+    {
+      path: '',
+      message: 'Something Went wrong',
+    },
+  ];
+
+  if(err instanceof ZodError) {
+   
+  }
+
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    errorSources,
+    Error : err
+  });
+};
+
+export default globalErrorHandler;
+
+// pattern
+/*
+ * success
+ * message
+ * errorSources : [
+ *   path : " ",
+ *   message : " "
+ * ]
+ * stack
+ */
