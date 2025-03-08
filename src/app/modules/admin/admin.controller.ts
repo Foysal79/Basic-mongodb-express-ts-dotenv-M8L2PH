1e0d458a7a4cfadce59f86ec85446c91b60ae7d2
Foysal79 <onthor312001@gmail.com>
2025-03-09 02:52:01 +0600

    admin get all data controller session create

--- src/app/modules/admin/admin.controller.ts
+++ src/app/modules/admin/admin.controller.ts
@@ -1,33 +1,28 @@
-import catchAsync from "../../utils/catchAsync";
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import { AdminServices } from './admin.service';
 
+const getAllAdmins = catchAsync(async (req, res) => {
+  const result = await AdminServices.getAllAdminFromDB(req.query);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Admins fetched successfully',
+    data: result,
+  });
+});
 
-const getAllAdmins = catchAsync(
-    async (req, res) => {
-        
-    }
-)
+const getSingleAdmin = catchAsync(async (req, res) => {
+    
+});
 
-const getSingleAdmin = catchAsync(
-    async (req, res) => {
+const updateAdmin = catchAsync(async (req, res) => {});
 
-    }
-)
-
-const updateAdmin = catchAsync(
-    async (req, res) => {
-
-    }
-)
-
-const deletedAdmin = catchAsync(
-    async (req, res) => {
-
-    }
-)
+const deletedAdmin = catchAsync(async (req, res) => {});
 
 export const AdminController = {
-    getAllAdmins,
-    getSingleAdmin,
-    updateAdmin,
-    deletedAdmin
-}
+  getAllAdmins,
+  getSingleAdmin,
+  updateAdmin,
+  deletedAdmin,
+};
--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -1,21 +1,88 @@
+import mongoose from 'mongoose';
 import QueryBuilder from '../../builder/QueryBuilder';
 import { AdminSearchableFields } from './admin.constant';
+import { TAdmin } from './admin.interface';
 import { Admin } from './admin.model';
+import AppError from '../../errors/AppError';
+import { User } from '../user/user.model';
 
 const getAllAdminFromDB = async (query: Record<string, unknown>) => {
   const adminQuery = new QueryBuilder(Admin.find(), query)
-  .search(
-    AdminSearchableFields,
-  )
-  .filter()
-  .sort()
-  .Pagination()
-  .fields()
+    .search(AdminSearchableFields)
+    .filter()
+    .sort()
+    .Pagination()
+    .fields();
 
   const result = await adminQuery.modelQuery;
   return result;
 };
 
-export const AdminServices = {
-    getAllAdminFromDB
+const getSingleAdminFromDB = async (id: string) => {
+  const result = await Admin.findById(id);
+  return result;
+};
+
+const updateAdminIntoDB = async (id: string, payload: Partial<TAdmin>) => {
+  const { name, ...remainingAdminData } = payload;
+
+  const modifiedUpdateData: Record<string, unknown> = {
+    ...remainingAdminData,
   };
+  if (name && Object.keys(name).length) {
+    for (const [key, value] of Object.entries(name)) {
+      modifiedUpdateData[`name${key}`] = value;
+    }
+  }
+  const result = await Admin.findByIdAndUpdate({ id }, modifiedUpdateData, {
+    new: true,
+    runValidators: true,
+  });
+
+  return result;
+};
+
+const deletedAdminFromDB = async (id: string) => {
+  const session = await mongoose.startSession();
+  try {
+    session.startTransaction();
+    const deletedAdmin = await Admin.findByIdAndUpdate(
+      id,
+      {
+        isDeleted: true,
+      },
+      {
+        new: true,
+        session,
+      },
+    );
+
+    if (!deletedAdmin) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to deleted admin');
+    }
+    // transaction 2
+    const userId = deletedAdmin.user;
+    const deletedUser = await User.findByIdAndUpdate(
+      userId,
+      { isDeleted: true },
+      { new: true, session },
+    );
+    if (!deletedUser) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to deleted user');
+    }
+    await session.commitTransaction();
+    await session.endSession();
+    return deletedAdmin;
+  } catch (error: any) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw new Error(error);
+  }
+};
+
+export const AdminServices = {
+  getAllAdminFromDB,
+  getSingleAdminFromDB,
+  updateAdminIntoDB,
+  deletedAdminFromDB,
+};
