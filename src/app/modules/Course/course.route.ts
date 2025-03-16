732b840f1d50b2dd95a48974569ebc64ca33e370
Foysal79 <onthor312001@gmail.com>
2025-03-17 01:06:54 +0600

    course routes create

---
+++ src/app/modules/Course/course.route.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import validateRequest from '../middlwares/validateRequest';
+import { CourseValidations } from './course.validation';
+import { CourseController } from './course.controller';
+
+const router = express.Router();
+
+router.post(
+  '/create-course',
+  validateRequest(CourseValidations.createCourseValidationSchema),
+  CourseController.createCourse,
+);
+
+router.get('/', CourseController.getAllCourses);
+router.get('/:id', CourseController.getSingleCourse);
+router.delete('/:id', CourseController.deletedCourse);
+
+export const CourseRoutes = router;
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,47 +1,46 @@
-
-
-import { Router } from "express"
-import { StudentRoutes } from "../modules/students/student.route"
-import { UserRoutes } from "../modules/user/user.route"
-import { AcademicSemesterRoutes } from "../modules/academicSemester/academicSemester.route"
-import { AcademicFacultyRoutes } from "../modules/academicFaculty/academicFaculty.route"
-import { AcademicDepartmentRouter } from "../modules/academicDepartment/academicDepartment.route"
-import path from "path"
-import { AdminRoutes } from "../modules/admin/admin.route"
-import { FacultyRouter } from "../modules/Faculty/faculty.route"
-const router = Router()
+import { Router } from 'express';
+import { StudentRoutes } from '../modules/students/student.route';
+import { UserRoutes } from '../modules/user/user.route';
+import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.route';
+import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.route';
+import { AcademicDepartmentRouter } from '../modules/academicDepartment/academicDepartment.route';
+import { AdminRoutes } from '../modules/admin/admin.route';
+import { FacultyRouter } from '../modules/Faculty/faculty.route';
+import { CourseRoutes } from '../modules/Course/course.route';
+const router = Router();
 const moduleRouter = [
-    {
-        path : '/student',
-        route : StudentRoutes,
-    },
-    {
-        path : '/users',
-        route : UserRoutes,
-    },
-    {
-        path : '/academic-semesters',
-        route : AcademicSemesterRoutes,
-        
-    },
-    {
-        path : '/academic-faculties',
-        route : AcademicFacultyRoutes,
-        
-    },
-    {
-        path : '/academic-department',
-        route : AcademicDepartmentRouter,
-        
-    }, 
-    {
-        path : '/admin',
-        route : AdminRoutes
-    }, 
-    {
-        path : '/faculty',
-        route : FacultyRouter
-    }
-]
-moduleRouter.forEach(route => router.use(route.path, route.route))
+  {
+    path: '/student',
+    route: StudentRoutes,
+  },
+  {
+    path: '/users',
+    route: UserRoutes,
+  },
+  {
+    path: '/academic-semesters',
+    route: AcademicSemesterRoutes,
+  },
+  {
+    path: '/academic-faculties',
+    route: AcademicFacultyRoutes,
+  },
+  {
+    path: '/academic-department',
+    route: AcademicDepartmentRouter,
+  },
+  {
+    path: '/admin',
+    route: AdminRoutes,
+  },
+  {
+    path: '/faculty',
+    route: FacultyRouter,
+  },
+  {
+    path: '/course',
+    route: CourseRoutes,
+  },
+];
+moduleRouter.forEach((route) => router.use(route.path, route.route));
 export default router;
